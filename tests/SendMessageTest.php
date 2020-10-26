ab56c2f25c3e2f0f004c5e47d90f4064ff91ba70
Jesse Kramer <jesse@kramerventures.nl>
2020-10-26 07:51:07 +0100

    Refactor Unit test, add assertions.

---
+++ tests/SendMessageTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace UptimeProject\Slack\Tests;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use UptimeProject\Slack\Exceptions\SlackMessageException;
+use UptimeProject\Slack\MessageDraft;
+use UptimeProject\Slack\Workspace;
+
+class SendMessageTest extends TestCase
+{
+    const EXPECTED_HTTP_METHOD = 'POST';
+    const EXPECTED_URL = 'https://webhook.test';
+
+    public function message_data(): array
+    {
+        return [
+            'test_send_message' => [
+                'John',         // username
+                'Hello world!', // message
+                null,           // icon
+                null,           // channel
+                200,            // http_response
+                null,           // exception
+                null,           // icon_is_url
+            ],
+            'test_send_message_icon_emoji' => [
+                'John',         // username
+                'Hello world!', // message
+                ':tada:',       // icon
+                null,           // channel
+                200,            // http_response
+                null,           // exception
+                false,          // icon_is_url
+            ],
+            'test_send_message_icon_url' => [
+                'John',         // username
+                'Hello world!', // message
+                'https://example.com/test.png', // icon
+                null,           // channel
+                200,            // http_response
+                null,           // exception
+                true,           // icon_is_url
+            ],
+            'test_send_message_to_channel' => [
+                'John',         // username
+                'Hello world!', // message
+                null,           // icon
+                '#general',     // channel
+                200,            // http_response
+                null,           // exception
+                null,           // icon_is_url
+            ],
+            'test_send_message_error' => [
+                'John',         // username
+                'Hello world!', // message
+                null,           // icon
+                null,           // channel
+                500,            // http_response
+                SlackMessageException::class, // exception
+                null,           // icon_is_url
+            ],
+        ];
+    }
+
+    /** @dataProvider message_data */
+    public function test_send_message(
+        string $username,
+        string $message,
+        ?string $icon,
+        ?string $channel,
+        int $httpResponse,
+        ?string $expectedException,
+        ?bool $iconIsUrl
+    ): void
+    {
+        $mock = new MockHandler([
+            new Response($httpResponse),
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+
+        $handlerStack->push(function (callable $handler) use ($username, $message, $icon, $channel, $iconIsUrl) {
+            return function (RequestInterface $request, array $options) use ($handler, $username, $message, $icon, $channel, $iconIsUrl) {
+                $assertMethod = SendMessageTest::EXPECTED_HTTP_METHOD;
+                $assertUrl = SendMessageTest::EXPECTED_URL;
+                // Make some assertions
+                $this->assertSame($assertUrl, (string) $request->getUri());
+                $this->assertSame(strtolower($assertMethod), strtolower($request->getMethod()));
+                $body = json_decode((string) $request->getBody(), true);
+                $this->assertSame($username, $body['username'] ?? null);
+                $this->assertSame($message, $body['text'] ?? null);
+                if (is_bool($iconIsUrl)) {
+                    $key = $iconIsUrl ? 'icon_url' : 'icon_emoji';
+                    $this->assertSame($icon, $body[$key] ?? null);
+                }
+                $this->assertSame($channel, $body['channel'] ?? null);
+                
+                // Go on with business.
+                return $handler($request, $options);
+            };
+        });
+
+        $client = new Client(['handler' => $handlerStack]);
+        $workspace = new Workspace(self::EXPECTED_URL);
+        $workspace->setClient($client);
+
+        $draft = $workspace->from($username, $icon);
+        $this->assertInstanceOf(MessageDraft::class, $draft);
+
+        if ($expectedException) {
+            $this->expectException($expectedException);
+        }
+        $draft->send($message, $channel);
+    }
+}
